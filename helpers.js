ecfe94030a691adadc1bb83a48e4786128157886
Administrator <mongolrgata@gmail.com>
2013-08-02 17:47:31 +0400

    +damnControl

--- helpers.js
+++ helpers.js
@@ -5,27 +5,53 @@
  * Time: 22:40
  */
 
-/**
- * Вызов метода БЛ
- * @param {string} fullMethodName полное имя метода (вместе с именем объекта БЛ через точку)
- * @param {Object} [params={}] аргументы
- * @param {'asis'|'record'|'recordset'} [type='asis'] тип результата
- * @param args
- * @returns {$ws.proto.Deferred}
- */
-function BLObjectC(fullMethodName, params, type, ...args) {
-    var splitName = fullMethodName.split('.');
-    return $ws.proto.ClientBLObject.prototype.call.apply(new $ws.proto.BLObject(splitName[0]), [splitName[1], params || {}, $ws.proto.BLObject['RETURN_TYPE_' + (type || 'ASIS').toUpperCase()]].concat(args));
-}
+(function () {
+    "use strict";
 
-/**
- * Вызов списочного метода БЛ
- * @param {string} fullMethodName полное имя списочного метода (вместе с именем объекта БЛ через точку)
- * @param {Object} [params={}] фильтр
- * @param args
- * @returns {$ws.proto.Deferred}
- */
-function BLObjectQ(fullMethodName, params, ...args) {
-    var splitName = fullMethodName.split('.');
-    return $ws.proto.ClientBLObject.prototype.query.apply(new $ws.proto.BLObject(splitName[0]), [splitName[1], params || {}].concat(args));
-}
+    var helpersMap = {
+        /**
+         * @param controlNameOrId имя или идентификатор
+         * @returns {$ws.proto.Control}
+         */
+        damnControl : function (controlNameOrId) {
+            if ($ws.single.ControlStorage.containsByName(controlNameOrId)) {
+                return $ws.single.ControlStorage.getByName(controlNameOrId);
+            }
+            if ($ws.single.ControlStorage.contains(controlNameOrId)) {
+                return $ws.single.ControlStorage.get(controlNameOrId);
+            }
+        },
+
+        /**
+         * Вызов метода БЛ
+         * @param {string} fullMethodName полное имя метода (вместе с именем объекта БЛ через точку)
+         * @param {Object} [params={}] аргументы
+         * @param {'asis'|'record'|'recordset'} [type='asis'] тип результата
+         * @param args
+         * @returns {$ws.proto.Deferred}
+         */
+        BLObjectC : function (fullMethodName, params, type, ...args) {
+            var splitName = fullMethodName.split('.');
+            return $ws.proto.ClientBLObject.prototype.call.apply(new $ws.proto.BLObject(splitName[0]), [splitName[1], params || {}, $ws.proto.BLObject['RETURN_TYPE_' + (type || 'ASIS').toUpperCase()]].concat(args));
+        },
+
+        /**
+         * Вызов списочного метода БЛ
+         * @param {string} fullMethodName полное имя списочного метода (вместе с именем объекта БЛ через точку)
+         * @param {Object} [params={}] фильтр
+         * @param args
+         * @returns {$ws.proto.Deferred}
+         */
+        BLObjectQ : function (fullMethodName, params, ...args) {
+            var splitName = fullMethodName.split('.');
+            return $ws.proto.ClientBLObject.prototype.query.apply(new $ws.proto.BLObject(splitName[0]), [splitName[1], params || {}].concat(args));
+        }
+    };
+
+    var global = (0 || eval)('this');
+    for (var name in helpersMap) {
+        if (helpersMap.hasOwnProperty(name)) {
+            global[name] = helpersMap[name];
+        }
+    }
+})();
